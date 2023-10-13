e8a4d65e85fbc4ad8d233f4669bd10fac98f226d
ThiagoPS <thiago.tpd10@gmail.com>
2023-10-13 17:27:19 -0300

    Complementação da classe Conta
    Foi criado alguns métodos getters e setters para a classe Conta. Assim como um método para fazer transferências entre 2 contas.

--- conta.java
+++ conta.java
@@ -2,7 +2,32 @@ class Conta{
     private int numero;
     private String titular;
     private double saldo;
-
+    
+    public String getTitular(){
+        String nomeTitular = this.titular;
+        return nomeTitular;
+    }
+    public void setTitular(String nome){
+        this.titular = nome;
+    }
+    
+    public int getNumero(){
+        int numeroConta = this.numero;
+        return numeroConta;
+    }
+    public void setNumero(int num){
+        if (num <= 0){
+            System.out.println("Não foi possível definer o número da conta! \nTente novamente mais tarde");
+        }else{
+            this.numero = num;
+            System.out.print("O número da conta foi definido com sucesso!");
+        }
+    }
+    
+    public double getSaldo(){
+        double meuSaldo = this.saldo;
+        return meuSaldo;
+    }
     public boolean deposita(double grana){
         if (grana < 0){
             return false;
@@ -12,7 +37,6 @@ class Conta{
             return true;
         }
     }
-
     public Boolean saca(double grana){
         if (this.saldo < grana){
             return false;
@@ -22,22 +46,22 @@ class Conta{
             return true;
         }
     }
-
-    public double getSaldo(){
-        double meuSaldo = this.saldo;
-        return meuSaldo;
-    }
-
-    public void setTitular(String nome){
-        this.titular = nome;
-    }
-
-    public void setNumero(int num){
-        if (num <= 0){
-            System.out.println("Não foi possível definer o número da conta! \nTente novamente mais tarde");
+    public boolean transfere(Conta destino, double grana){
+        if (grana < 0){
+            return false;
         }else{
-            this.numero = num;
-            System.out.print("O número da conta foi definido com sucesso!");
+            Double minhaGrana = this.saldo;
+            if (minhaGrana < grana){
+                return false;
+            }else{
+                minhaGrana -= grana;
+                this.saldo = minhaGrana;
+
+                destino.deposita(grana);
+
+                return true;
+            }
         }
     }
+
 }
